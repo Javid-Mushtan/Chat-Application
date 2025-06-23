3b14086c69d8b28eb4cf6d894399c887ae131ba3
Javid Mushtan <javidmushtan@gmail.com>
2025-06-23 21:06:07 +0530

    constructed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,15 +1,15 @@
 import User from "../models/user.model.js";
 import {generateToken} from "../lib/utils.js";
-const bcrypt = require("bcryptjs");
+import bcrypt from "bcryptjs";
 
 export const signup = async (req, res) => {
-    const {fullName, email, password} = req.body;
+    const { fullName, email, password } = req.body;
     try {
         if (password.length < 6) {
             return res.status(400).json({message: "Password must be at least 6 characters"});
         }
 
-        const user = await User.find({ email });
+        const user = await User.findOne({ email });
 
         if(user) {
             return res.status(400).json({message: "Email already exists"});
@@ -19,23 +19,56 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const newUser = new User({
-            fullName: fullName,
-            email: email,
+            fullName,
+            email,
             password: hashedPassword,
         });
 
         if(newUser) {
-            generateToken(newUser._id,res);
             await newUser.save();
+            generateToken(newUser._id,res);
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
         } else {
-            return res.status(401).json({message: "Invalid Credentials"});
+            return res.status(400).json({message: "Invalid Credentials"});
         }
-    } catch (err) {
-        res.status(400).send({})
+    } catch (error) {
+        console.log("Error in signup controller " + error.message);
+        res.status(400).send({message: "Invalid Credentials"});
     }
 };
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        generateToken(user._id,res);
+
+        res.status(201).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic,
+        });
+    } catch (error) {
+        console.log("Error in signup controller " + error.message);
+        res.status(400).send({message: "Invalid Credentials"});
+    }
 };
 export const logout = (req, res) => {
     res.send("logout route");
