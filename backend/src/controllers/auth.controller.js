791aacdeb3d13c15413e3a56ad5705b8cea70167
Javid Mushtan <javidmushtan@gmail.com>
2025-06-24 19:47:46 +0530

    constructed 1

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import {generateToken} from "../lib/utils.js";
 import bcrypt from "bcryptjs";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
     const { fullName, email, password } = req.body;
@@ -43,6 +44,7 @@ export const signup = async (req, res) => {
         res.status(400).send({message: "Invalid Credentials"});
     }
 };
+
 export const login = async (req, res) => {
     const { email, password } = req.body;
     try {
@@ -70,6 +72,44 @@ export const login = async (req, res) => {
         res.status(400).send({message: "Invalid Credentials"});
     }
 };
+
 export const logout = (req, res) => {
-    res.send("logout route");
+    try{
+        res.cookie("jwt","", { maxAge: 0 });
+        res.status(200).json({message: "Logged out successfully"});
+    } catch(error) {
+        console.log("Error in logout controller " + error.message);
+        res.status(500).send({message: "Invalid Credentials"});
+    }
 };
+
+export const updateProfile = async (req, res) => {
+    try{
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if(!profilePic) {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePic: uploadResponse.secure_url },
+            { new: true }
+        );
+
+    } catch(error) {
+        console.log("error in update profile: ", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+
+export const checkAuth = async (req, res) => {
+    try{
+        res.status(200).json(req.user);
+    }catch(error) {
+        console.log("Error in checkAuth controller " + error.message);
+        res.status(500).send({message: "Internal server error"});
+    }
+}
